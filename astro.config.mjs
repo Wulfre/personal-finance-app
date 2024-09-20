5db0864ce5d4d468d7d593fadbb308b8ae8501f3
Wulfre <wulfre@users.noreply.github.com>
2024-09-20 14:28:16 -0400

    initialize astro project

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import cloudflare from "@astrojs/cloudflare"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+export default defineConfig({
+	output: "server",
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+		imageService: "passthrough",
+	}),
+	integrations: [tailwind()],
+})
